210208c918225eb26e248b032790c9c3d47f4f40
levik <zhenya12@gmail.com>
2022-07-25 20:39:53 +0300

    change url.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/test_website/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/techworld-with-yevgen/" : "/",
   css: {
     loaderOptions: {
       css: {
